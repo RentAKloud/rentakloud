d4b2ed5eae7bfa19de95782f96bda6322e4e3f67
Mustaghees Butt <renaldpirate@gmail.com>
2023-06-04 04:00:53 +0500

    feat(anim): improved cloud animation

--- web/src/components/CloudsAnimation.tsx
+++ web/src/components/CloudsAnimation.tsx
@@ -1,10 +1,15 @@
 import { Component, For, createSignal, onCleanup } from "solid-js"
 
 const CloudsAnimation: Component = () => {
-  const [clouds, setClouds] = createSignal([...Array(20).keys()].map((c) => {
-    const y = Math.random() * (window.screen.height - 100)
-    const x = Math.random() * window.innerWidth
-    const speed = Math.random() * 2.5 + 1
+  const sectionHeight = window.screen.height - 150
+  const sectionWidth = window.innerWidth
+  const numClouds = 20
+  const cloudSpacing = sectionHeight / numClouds
+
+  const [clouds, setClouds] = createSignal([...Array(numClouds).keys()].map((c) => {
+    const y = c * cloudSpacing
+    const x = Math.random() * sectionWidth
+    const speed = Math.random() * 1.618 + 1
     return {
       x, y,
       top: y,
@@ -17,16 +22,18 @@ const CloudsAnimation: Component = () => {
 
   function myAnim(dt: number) {
     const newClouds = clouds().map(c => {
-      const max = c.x + 200
-      if (c.dir === 1 && c.left >= max) {
-        c.dir = -1
-      } else if (c.dir === -1 && c.left < c.x) {
-        c.dir = 1
-      }
+      // Alternating direction
+      // const maxX = c.x + 250
+      // if (c.dir === 1 && c.left >= maxX) {
+      //   c.dir = -1
+      // } else if (c.dir === -1 && c.left < c.x) {
+      //   c.dir = 1
+      // }
 
       return {
         ...c,
-        left: c.left + c.speed * c.dir,
+        left: (c.left + c.speed * c.dir) % sectionWidth,
+        top: c.top + Math.sin(c.left / 50) * 2 * c.dir,
         dir: c.dir
       }
     })
@@ -41,11 +48,11 @@ const CloudsAnimation: Component = () => {
   })
 
   return (
-    <div class="absolute w-full h-full flex flex-col justify-between opacity-70">
+    <div class="absolute w-full h-full flex flex-col justify-between opacity-50 overflow-hidden pointer-events-none">
       <For each={clouds()}>
         {(c) => {
           return (
-            <div class="absolute" style={{
+            <div class="absolute invert dark:invert-0" style={{
               left: c.left + "px",
               top: c.top + "px",
               "font-size": c.size + "px"
