8a63e0d9c5de528f7781a39f180a37c82ecceedf
Mustaghees Butt <renaldpirate@gmail.com>
2023-08-26 02:37:34 +0500

    fix(caddy): correct paths for static sites + add CORS for production

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,17 +6,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:3001"]
+    origin: ["http://localhost:3001", "https://rentakloud.com"]
   })
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('RentAKloud API')
     .setDescription('Documentation for RentAKloud API.')
     .setVersion('1.0')
     // .addTag('cloud')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
