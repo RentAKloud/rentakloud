acfe95b529b66f3d1fe88dacfbe047c0cc2534a6
Mustaghees Butt <renaldpirate@gmail.com>
2023-11-19 20:49:26 +0500

    feat(orders): Customer can cancel orders

--- web/src/pages/Dashboard/Orders.tsx
+++ web/src/pages/Dashboard/Orders.tsx
@@ -1,12 +1,18 @@
-import { Component, For, Show, createResource } from "solid-js";
+import { Component, For, Show, createResource, createSignal } from "solid-js";
 import { Link } from "@solidjs/router";
 import OrdersApi from "~/api/orders";
 import Loader from "~/components/Loader";
 import { DateTime } from "~/components/DateTime";
 import { formatPrice } from "~/utils";
+import Modal from "~/components/Modal";
+import { Order, OrderStatus } from "~/types/order";
+import { NotificationService } from "~/services/NotificationService";
 
 const Orders: Component = () => {
-  const [orders] = createResource(async () => {
+  const [isCancelModalOpen, setIsCancelModalOpen] = createSignal(false)
+  const [selectedOrder, setSelectedOrder] = createSignal<Order | null>(null)
+
+  const [orders, { refetch }] = createResource(async () => {
     const { result, error } = await OrdersApi.all()
     if (error) {
       throw error
@@ -14,6 +20,15 @@ const Orders: Component = () => {
 
     return result
   })
+
+  async function cancelOrder(id: number) {
+    const { error } = await OrdersApi.updateStatus(id, OrderStatus.Cancelled)
+    if (!error) {
+      NotificationService.success(`Order #${id} was cancelled successfully!`)
+      setTimeout(refetch, 1000)
+    }
+    setIsCancelModalOpen(false)
+  }
 
   return (
     <>
@@ -54,8 +69,14 @@ const Orders: Component = () => {
                         <td><DateTime value={order.createdAt} /></td>
                         <td>{formatPrice(order.amount || 0)}</td>
                         <td>{order.status}</td>
-                        <td>
+                        <td class="flex gap-4">
                           <Link href={`/dashboard/orders/${order.id}`} class="btn btn-info btn-xs link-hover">Details</Link>
+                          <Show when={[OrderStatus.Pending, OrderStatus.OnHold, OrderStatus.Paid].includes(order.status)}>
+                            <button class="btn btn-error btn-xs" onclick={() => {
+                              setSelectedOrder(order)
+                              setIsCancelModalOpen(true)
+                            }}>Cancel</button>
+                          </Show>
                         </td>
                       </tr>
                     )
@@ -66,6 +87,18 @@ const Orders: Component = () => {
           </table>
         </div>
       </Show>
+
+      <Modal
+        isOpen={isCancelModalOpen()}
+        onClose={() => setIsCancelModalOpen(false)}
+        title="Confirm Cancellation"
+        description={`Are you sure you want to cancel order #${selectedOrder()!.id}?`}
+        actions={
+          <>
+            <button class="btn" onclick={() => setIsCancelModalOpen(false)}>No</button>
+            <button class="btn btn-error" onclick={() => cancelOrder(selectedOrder()!.id)}>Yes</button>
+          </>
+        } />
     </>
   )
 }
