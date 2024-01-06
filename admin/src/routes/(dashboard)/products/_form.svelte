a8908e30d5e204d72f8a1aa10530e176cfc6584f
Mustaghees Butt <renaldpirate@gmail.com>
2024-01-07 01:13:07 +0500

    fix(admin/products): fix price cloning + refactor

--- admin/src/routes/(dashboard)/products/_form.svelte
+++ admin/src/routes/(dashboard)/products/_form.svelte
@@ -17,6 +17,7 @@
   import Editor from "../../../components/Editor.svelte";
   import { onMount } from "svelte";
   import { Http } from "$lib/http";
+    import Price from "./_price.svelte";
 
   export let product: Product;
   export let onSubmit: (data: Product) => Promise<void>;
@@ -129,92 +130,7 @@
       </div>
 
       {#each product.prices as price, i}
-        <div class="flex gap-4 mb-4">
-          <div class="flex flex-col gap-2">
-            <Button
-              outline
-              on:click={() =>
-                (product.prices = product.prices.filter((p, y) => y !== i))}
-            >
-              <TrashBinSolid />
-            </Button>
-
-            <Button
-              outline
-              on:click={() => (product.prices = [...product.prices, price])}
-            >
-              <CopySolid />
-            </Button>
-          </div>
-
-          <div class="flex flex-col gap-4 mb-4">
-            {#if isOnlineService}
-              <Input placeholder="Plan name" bind:value={price.planName} />
-            {/if}
-            <div class="flex gap-4">
-              <Input
-                type="number"
-                step="0.01"
-                placeholder="Price"
-                bind:value={price.amount}
-              />
-              <Input
-                type="number"
-                step="0.01"
-                placeholder="Sale Price"
-                bind:value={price.saleAmount}
-              />
-              <Input placeholder="Currency" bind:value={price.currency} />
-
-              {#if isOnlineService}
-                <Select
-                  items={[
-                    { value: "month", name: "Monthly" },
-                    { value: "year", name: "Yearly" },
-                  ]}
-                  bind:value={price.interval}
-                />
-              {/if}
-            </div>
-
-            {#if isOnlineService}
-              <div>
-                <div class="flex gap-4 items-center mb-2">
-                  <Label>Features</Label>
-                  <Button
-                    pill
-                    class="!p-2"
-                    size="sm"
-                    on:click={() => {
-                      if (price.features) {
-                        price.features = [...price.features, ""];
-                      } else {
-                        price.features = [""];
-                      }
-                    }}
-                  >
-                    <PlusSolid class="dark:text-white" />
-                  </Button>
-                </div>
-
-                {#each price.features || [] as feature, j}
-                  <div class="flex gap-4">
-                    <Input placeholder="Feature" bind:value={feature} />
-                    <Button
-                      outline
-                      on:click={() =>
-                        (price.features = (price.features || []).filter(
-                          (p, y) => y !== j,
-                        ))}
-                    >
-                      <TrashBinSolid />
-                    </Button>
-                  </div>
-                {/each}
-              </div>
-            {/if}
-          </div>
-        </div>
+        <Price bind:prices={product.prices} {price} {i} {isOnlineService} />
       {/each}
     </div>
 
